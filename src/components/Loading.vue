bf407c8dc2ea5d59fe7dcb591f5ca572f310be50
renchaochao <ren709271967@163.com>
2017-11-02 16:27:54 +0800

    新增

---
+++ src/components/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="load">
+     <img src="../assets/images/loading.gif" class="load_img" />
+  </div>
+</template>
+<script>
+export default{
+    name:'loading',
+	data(){
+	   return {
+	   }
+	}
+}
+</script>
+<style lang="scss" scoped>
+    .load{ 
+	  width:90%;
+	  margin:0 auto;
+	  height:100%;
+	  overflow:hidden;
+	  .load_img{margin:0px auto;padding:0px;overflow:hidden;display:block;width:100%;}
+	}
+</style>
---
+++ src/components/Scroll.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'scroll',
+  props:{
+     probeType:{
+	   type:Number,
+	   default:1
+	 },
+	 click:{
+	   type:Boolean,
+	   default:true
+	 },
+	 data:{
+	   type: Array,
+	   dafault:null
+	 },
+	 listenScroll: {
+        type: Boolean,
+        default: false
+      },
+      listenBeforeScroll: {
+        type: Boolean,
+        default: false
+      },
+      scrollbar: {
+        type: null,
+        default: false
+      },
+      pullDownRefresh: {
+        type: null,
+        default: false
+      },
+      pullUpLoad: {
+        type: null,
+        default: false
+      },
+      startY: {
+        type: Number,
+        default: 0
+      },
+      refreshDelay: {
+        type: Number,
+        default: 20
+      },
+      freeScroll: {
+        type: Boolean,
+        default: false
+      }
+    },
+	mounted(){
+	  setTimeout(()=>{
+	     this.initScroll();
+	  },20)
+	},
+	methods:{
+	  initScroll(){
+	     if(!this.$refs.wrapper){
+		    return;
+		 }
+	     this.scroll=new BScroll(this.$refs.wrapper,{
+		    probeType: this.probeType,
+            click: this.click
+		    }
+		 )
+		// 是否派发滚动事件 
+		if (this.listenScroll) {
+			let me = this;
+			this.scroll.on('scroll', (pos) => {
+		      me.$emit('scroll', pos) }) 
+		    }
+	    
+		// 是否派发滚动到底部事件，用于上拉加载 
+		
+		if (this.pullUpLoad) {
+		    let me = this;
+			this.scroll.on('scrollEnd', () => {
+			// 滚动到底部 
+			if (this.scroll.y <= (this.scroll.maxScrollY + 50)) { 
+			     me.$emit('scrollToEnd') 
+				 } 
+			}) 
+		}
+		 // 是否派发顶部下拉事件，用于下拉刷新		 
+		if (this.pullDownRefresh) {
+		    let me = this;
+     		this.scroll.on('touchEnd', (pos) => { 
+			  // 下拉动作 
+			  if (pos.y > 50) {
+			  me.$emit('pulldown') 
+			  } 
+			}) 
+		}
+	}, 
+	  disable() {
+        this.scroll && this.scroll.disable()
+      },
+      enable() {
+        this.scroll && this.scroll.enable()
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh()
+      },
+      scrollTo() {
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement() {
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+      },
+      clickItem(e, item) {
+        console.log(e)
+        this.$emit('click', item)
+      },
+      destroy() {
+        this.scroll.destroy()
+      }
+	},
+	watch: {
+      data() {
+        setTimeout(() => {
+          this.refresh();
+        }, this.refreshDelay)
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .wrapper{
+    height:500px;
+  }
+</style>
